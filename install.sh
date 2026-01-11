199f957a91013edc2788ec49c2e8cc1f8963f74a
fchouteau <florient.chouteau@gmail.com>
2026-01-11 15:17:51 +0100

    update devcontainer install config 📦️

--- install.sh
+++
@@ -1,3 +0,0 @@
-echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install -y google-cloud-cli ffmpeg libsm6 libxext6
